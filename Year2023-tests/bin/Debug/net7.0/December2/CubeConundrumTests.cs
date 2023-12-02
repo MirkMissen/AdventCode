cc3944a26b9838895ba804078960e3db89572176
MirkMissen <Mortenkristensen13@gmail.com>
2023-12-02 23:32:47 +0100

    Inital commit + December 2 - 2 stars

---
+++ Year2023-tests/bin/Debug/net7.0/December2/CubeConundrumTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit.Abstractions;
+using Year2023.December2;
+
+namespace Year2023_tests.December2; 
+
+public class CubeConundrumTests {
+    
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public CubeConundrumTests(ITestOutputHelper testOutputHelper) {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    private const string DataFilePath = "December2/GameData1.txt";
+    private const string TestCase1Path = "December2/TestCase1.txt";
+
+    [Fact]
+    public void TestCaseOne() {
+        var setConfig = new SetConfiguration(12, 13, 14);
+        var data = GameDataParser.ParseContent(TestCase1Path);
+
+        var result = data.CalculateSumOfPossiblesIdentifiers(setConfig);
+        
+        Assert.Equal(8, result);
+    }
+
+    [Fact]
+    public void CalculateMinimumSet() {
+        var data = GameDataParser.ParseContent(TestCase1Path);
+
+        var result = data.CalculateMinimumSetPower();
+        
+        Assert.Equal(2286, result);
+    }
+    
+    [Fact]
+    public void Calculate() {
+
+        var setConfig = new SetConfiguration(12, 13, 14);
+        var data = GameDataParser.ParseContent(DataFilePath);
+
+        var result = data.CalculateSumOfPossiblesIdentifiers(setConfig);
+        var minimumSetResult = data.CalculateMinimumSetPower();
+
+        _testOutputHelper.WriteLine($"result: {result}");
+        _testOutputHelper.WriteLine($"Minimum set result: {minimumSetResult}");
+    }
+    
+}
---
+++ Year2023/December2/CubeConundrum.cs
@@ -0,0 +1,93 @@
+﻿using Optional.Collections;
+
+namespace Year2023.December2; 
+
+public static class CubeConundrum {
+
+    public static int CalculateMinimumSetPower(this IEnumerable<GameData> gameData) {
+        return gameData
+            .Select(x => x.CalculateMinimumSet())
+            .Select(x => x.BlueCubes * x.GreenCubes * x.RedCubes)
+            .Sum();
+    }
+
+    private static Draw CalculateMinimumSet(this GameData gameData) {
+        var allDraws = gameData.Draws.ToArray();
+        
+        var minRed = allDraws.Select(x => x.RedCubes).Max();
+        var minBlue = allDraws.Select(x => x.BlueCubes).Max();
+        var minGreen = allDraws.Select(x => x.GreenCubes).Max();
+        return new Draw(minRed, minGreen, minBlue);
+    }
+    
+    public static int CalculateSumOfPossiblesIdentifiers(this IEnumerable<GameData> gameData, SetConfiguration setConfiguration) {
+        return gameData
+            .Where(x => x.IsGamePossible(setConfiguration))
+            .Sum(x => x.Id);
+    }
+
+    private static bool IsGamePossible(this GameData gameData, SetConfiguration setConfiguration) {
+        return gameData.Draws.All(x => x.IsDrawPossible(setConfiguration));
+    }
+
+    private static bool IsDrawPossible(this Draw draw, SetConfiguration setConfiguration) {
+        return draw.BlueCubes <= setConfiguration.BlueCubes &&
+               draw.RedCubes <= setConfiguration.RedCubes &&
+               draw.GreenCubes <= setConfiguration.GreenCubes;
+    }
+    
+}
+
+public class GameDataParser {
+    
+    public static IEnumerable<GameData> ParseContent(string path) {
+        var data = File.ReadAllLines(path);
+
+        return data.Select(x => {
+            var split = x.Split(": ");
+
+            var gameId = ExtractGameId(split[0]);
+
+            var draws = split[1].Split("; ")
+                .Select(ExtractDraw);
+
+            return new GameData(gameId, draws);
+        });
+    }
+
+    private static Draw ExtractDraw(string text) {
+        // Count color, Count color, Count color
+        var splits = text.Split(", ");
+
+        var dictionary = splits.Select(x => x.Split(" "))
+            .ToDictionary(strings => strings[1], strings => int.Parse(strings[0]));
+
+        dictionary.TryGetValue("red", out var red);
+        dictionary.TryGetValue("green", out var green);
+        dictionary.TryGetValue("blue", out var blue);
+        
+        return new Draw(red, green, blue);
+    }
+    
+    private static int ExtractGameId(string game) {
+        var number = game.Replace("Game", "").Trim();
+        return int.Parse(number);
+    }
+    
+}
+
+public record GameData(
+    int Id,
+    IEnumerable<Draw> Draws
+    );
+
+public record Draw(
+    int RedCubes,
+    int GreenCubes,
+    int BlueCubes);
+
+public record SetConfiguration(
+    int RedCubes,
+    int GreenCubes,
+    int BlueCubes
+);
